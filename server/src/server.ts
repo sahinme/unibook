c6240f6da7675f5bbef77f346d38abeeaaccca23
Muhammed Enes Şahin <40808896+freeadmiral@users.noreply.github.com>
2019-04-14 19:13:18 +0300

    setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,20 @@
-import express from "express";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import TYPES from "./types";
+import container from "./inversify.config";
+import { logger } from "./util/Logger";
+import { RegistrableController } from "./controller/RegisterableController";
 
 // create express application
 const app: express.Application = express();
 // let express support JSON bodies
+app.use(bodyParser.json());
+
+// grabs the Controller from IoC container and registers all the endpoints
+const controllers: RegistrableController[] = container.getAll<
+  RegistrableController
+>(TYPES.Controller);
+controllers.forEach(controller => controller.register(app));
 
 // setup express middleware logging and error handling
 app.use(function(
@@ -11,6 +23,7 @@ app.use(function(
   res: express.Response,
   next: express.NextFunction
 ) {
+  logger.error(err.stack);
   next(err);
 });
 
@@ -23,6 +36,6 @@ app.use(function(
   res.status(500).send("Internal Server Error");
 });
 
-app.listen(5000, function() {
-  console.log("listening");
+app.listen(3000, function() {
+  logger.info("Example app listening on port 3000!");
 });
---
+++ server/src/util/Logger.ts
@@ -0,0 +1,6 @@
+import { Logger, LoggerInstance, LoggerOptions, transports } from "winston";
+
+export const logger: LoggerInstance = new Logger(<LoggerOptions>{
+  exitOnError: false,
+  transports: [new transports.Console()]
+});
